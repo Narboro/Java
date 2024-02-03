ad243b5c56d8f050f08d2db2ca99b52cb52efd85
Narboro <71980572+Narboro@users.noreply.github.com>
2024-02-03 23:51:49 +0300

    Update Counter.java

--- Counter/Counter.java
+++ Counter/Counter.java
@@ -12,33 +12,3 @@ public class Counter {
         }
     }
 }
-
-//class Operator {
-//    int counter;
-//    String counterName;
-//    Operator(int cnt, String name){
-//        this.counter = cnt;
-//        this.counterName = name;
-//    }
-////    Operator(String name){
-//    Operator(String counterName){
-////        this.counter = 0;
-////        this.counterName = name;
-//        this(0, counterName);
-//    }
-//    void incrementCounter(int number){
-//        this.counter += number;
-////        return this.counter;
-//        System.out.println(this.counterName + ": " + this.counter);
-//    }
-//    void inrement(){
-//        this.counter++;
-////        return this.counter;
-//        System.out.println(this.counterName + ": " + this.counter);
-//    }
-//    void decrement(){
-//        this.counter--;
-////        return this.counter;
-//        System.out.println(this.counterName + ": " + this.counter);
-//    }
-//}
