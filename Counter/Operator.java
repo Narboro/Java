0a658766c91b18ea1ff1b0e4de65ec24842ca5ed
Narboro <71980572+Narboro@users.noreply.github.com>
2024-02-03 23:52:11 +0300

    Update Operator.java

--- Counter/Operator.java
+++ Counter/Operator.java
@@ -5,25 +5,19 @@ class Operator {
         this.counter = cnt;
         this.counterName = name;
     }
-    //    Operator(String name){
     Operator(String counterName){
-//        this.counter = 0;
-//        this.counterName = name;
         this(0, counterName);
     }
     void incrementCounter(int number){
         this.counter += number;
-//        return this.counter;
         System.out.println(this.counterName + ": " + this.counter);
     }
     void inrement(){
         this.counter++;
-//        return this.counter;
         System.out.println(this.counterName + ": " + this.counter);
     }
     void decrement(){
         this.counter--;
-//        return this.counter;
         System.out.println(this.counterName + ": " + this.counter);
     }
 }
